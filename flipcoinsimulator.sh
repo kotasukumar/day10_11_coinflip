8d5646e786138040224b5a1979973e1143eaf9dc
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-02-24 12:27:55 +0530

    added:looping logic

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -1,11 +1,18 @@
 #program of fliping coin suimulator for github
-coin=$((RANDOM%2))
 head_count=0
 tail_count=0
-	if(($coin==0))
-	then
-		echo "it is head"
-	elif(($coin==1))
-	then
-		echo "it is tail"
-	fi
+	for((i=0;i<10;i++))
+	do
+	coin=$((RANDOM%2))
+		if(($coin==0))
+		then
+			echo "it is head"
+			((head_count++))
+		elif(($coin==1))
+		then
+			echo "it is tail"
+			((tail_count++))
+		fi
+	done
+echo "no.of head is:" $head_count
+echo "no.of tail is:" $tail_count
